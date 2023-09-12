d830e16bc1a6160456f3e0cc2dea46eaf80015a5
jangjoonmo <jjmo97@ajou.ac.kr>
2023-09-12 21:11:12 +0900

    feat: 섹션과 아이템 정의
    Compositional Layout을 위한 섹션 구조체와 아이템 열거형 정의

--- BasicCompositionalLayout/ViewController.swift
+++ BasicCompositionalLayout/ViewController.swift
@@ -17,3 +17,29 @@ class ViewController: UIViewController {
 
 }
 
+
+
+//섹션과 아이템 정의
+
+struct Section: Hashable {
+    let id: String
+}
+
+enum Item: Hashable {
+    case banner(HomeItem)
+    case normalCarousel(HomeItem)
+    case listCarousel(HomeItem)
+}
+
+struct HomeItem: Hashable {
+    let title: String
+    let subTitle: String?
+    let imageUrl: String
+}
+
+
+//컬렉션뷰 cell UI - 등록
+//레이아웃 구현 - 3가지
+//datasource -> cellProvider
+//snapshot -> datasource.apply(snapshot)
+
